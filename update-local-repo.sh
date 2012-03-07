7f49e814c8f7bd7dc0c5a089654811e1e00340f7
Jim Barritt <jim@planet-ix.com>
2012-03-07 14:46:10 +0000

    [jmdb] Adding a script to update the local repo

---
+++ update-local-repo.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+cp target/dependency/json-parser-1.0-SNAPSHOT.jar repo/org/ixcode/json-parser/1.0-SNAPSHOT/
+
+cp target/dependency/platform.core-1.0-SNAPSHOT.jar repo/org/ixcode/platform.core/1.0-SNAPSHOT/
+cp target/dependency/platform.http-1.0-SNAPSHOT.jar repo/org/ixcode/platform.http/1.0-SNAPSHOT/
