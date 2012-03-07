f857a601aadefd16eec67d153cfff4bc5e3c5784
Jim Barritt <jim@planet-ix.com>
2012-03-07 14:20:42 +0000

    [jmdb] Adding Heroku Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+java -cp target/classes:target/dependency/* org.ixcode.hyperspy.app.HyperSpyApp localhost 8765 ./target/web-resources
--- src/main/java/org/ixcode/hyperspy/app/HyperSpyApp.java
+++ src/main/java/org/ixcode/hyperspy/app/HyperSpyApp.java
@@ -1,22 +1,32 @@
 package org.ixcode.hyperspy.app;
 
-import ixcode.platform.http.protocol.IanaContentType;
 import ixcode.platform.http.server.HttpServer;
 import ixcode.platform.http.server.RequestDispatcher;
 import ixcode.platform.http.server.resource.RouteMap;
 
 import static ixcode.platform.http.protocol.HttpMethod.GET;
+import static ixcode.platform.http.protocol.IanaContentType.json;
 import static ixcode.platform.http.server.RequestDispatcher.requestDispatcher;
 import static ixcode.platform.http.server.resource.RouteMap.aResourceMapRootedAt;
 import static ixcode.platform.logging.ConsoleLog4jLogging.initialiseLog4j;
+import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
 
 public class HyperSpyApp {
     public static void main(String[] args) {
         initialiseLog4j();
-        RequestDispatcher requestDispatcher = requestDispatcher("http://localhost:9393", withRouteMap(), IanaContentType.json);
 
-        new HttpServer(HyperSpyApp.class.getSimpleName(), "localhost", 9393, requestDispatcher)
-                .servingStaticContentFrom("./")
+        boolean useDefult = args.length == 0;
+
+        String hostname = (useDefult) ? "localhost" : args[0];
+        int port = (useDefult) ? 9393 : parseInt(args[1]);
+        String webRoot = (useDefult) ? "./" : args[2];
+
+        RequestDispatcher requestDispatcher = requestDispatcher(
+                format("http://%s:%d", hostname, port), withRouteMap(), json);
+
+        new HttpServer(HyperSpyApp.class.getSimpleName(), hostname, port, requestDispatcher)
+                .servingStaticContentFrom(webRoot)
                 .start();
     }
 
